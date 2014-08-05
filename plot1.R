555d4a6f2df004a1301ae4bb44f584a3c9dc9f4b
estalker <sergei.knyazev@gmail.com>
2014-08-05 15:04:36 +0400

    first graph

---
+++ plot1.R
@@ -0,0 +1,8 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile="exdata_data_household_power_consumption.zip")
+unzip("exdata_data_household_power_consumption.zip")
+data<-read.csv('household_power_consumption.txt', header = TRUE, sep = ";", na.strings = "?")
+data_subs <- subset(data, as.Date(data$Date,"%d/%m/%Y")>="2007-02-01" & as.Date(data$Date,"%d/%m/%Y")<="2007-02-02")
+png("plot1.png")
+par(bg="transparent")
+hist(data_subs$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)", col="red", freq=TRUE, bg="transparent")
+dev.off()
